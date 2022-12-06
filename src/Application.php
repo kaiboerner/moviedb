dce5b59d82c4948c2b2d45a33a993308c9d38b9b
Kai Börner <kai.boerner@web.de>
2022-12-06 22:17:50 +0100

    Application class finalized Default Controller

--- src/Application.php
+++ src/Application.php
@@ -2,6 +2,9 @@
 
 namespace KaiBoerner\MovieDb;
 
+use KaiBoerner\MovieDb\Controller\IndexController;
+
+
 /**
  * entry point of the moviedb application
  */
@@ -9,6 +12,14 @@ final class Application
 {
     public function run(): void
     {
-        echo __METHOD__;
+        $action = empty($_REQUEST['action']) ? 'index' : strtolower($_REQUEST['action']);
+        # new_movie should become NewMovie
+        $action = implode('', array_map('ucfirst', preg_split('/[^a-z0-9]/', $action)));
+        $controller = getContainer()->get("\\KaiBoerner\\MovieDb\\Controller\\${action}Controller");
+
+        $task = empty($_REQUEST['task']) ? 'index' : strtolower($_REQUEST['task']);
+        $task = lcfirst(implode('', array_map('ucfirst', preg_split('/[^a-z0-9]/', $task))));
+
+        $controller->{$task . 'Action'}();
     }
 }
---
+++ src/Controller/IndexController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace KaiBoerner\MovieDb\Controller;
+
+
+final class IndexController
+{
+    public function indexAction(): void
+    {
+        echo __METHOD__;
+    }
+}
