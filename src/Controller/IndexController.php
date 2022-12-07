f07e53cb5f65d411812b0976d7fc1206d9b2cee0
Kai Börner <kai.boerner@web.de>
2022-12-07 19:55:19 +0100

    IndexController

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,11 +2,39 @@
 
 namespace KaiBoerner\MovieDb\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+use KaiBoerner\MovieDb\Entity\Movie;
+use KaiBoerner\MovieDb\Security\SecurityInterface;
+use KaiBoerner\MovieDb\Templating\TemplateEngine;
 
+/**
+ * Controller for list of movies
+ */
 final class IndexController
 {
+    const LIST_SIZE = 20;
+
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private SecurityInterface $security,
+        private TemplateEngine $templateEngine
+    )
+    {}
+
     public function indexAction(): void
     {
-        echo __METHOD__;
+        $templateVars = ['listSize' => self::LIST_SIZE];
+        $repository = $this->entityManager->getRepository(Movie::class);
+        $templateVars['count'] = $repository->count([]);
+        $templateVars['pages'] = max(1, ceil($templateVars['count'] / $templateVars['listSize']));
+        $templateVars['page'] = min($templateVars['pages'], max(1, isset($_REQUEST['page']) ? intval($_REQUEST['page']) : 1));
+        $templateVars['movies'] = $repository->createQueryBuilder('m')
+            ->orderBy('m.title', 'asc')
+            ->setFirstResult($templateVars['listSize'] * ($templateVars['page'] - 1))
+            ->setMaxResults($templateVars['listSize'])
+            ->getQuery()
+            ->getResult()
+        ;
+        $this->templateEngine->render('index/index.html', $templateVars);
     }
 }
--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -6,57 +6,30 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * Movie
- *
- * @ORM\Table(name="movie", indexes={@ORM\Index(name="IDX_1D5EF26FDE12AB56", columns={"created_by"})})
- * @ORM\Entity
  */
+ #[ORM\Table(name: "movie")]
+ #[ORM\Entity]
 class Movie
 {
-    /**
-     * @var int|null
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
+    #[ORM\Column(name: "id", type: "integer", nullable: false, options: array("unsigned" => true))]
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: "IDENTITY")]
     private ?int $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="title", type="string", length=255, nullable=false)
-     */
+    #[ORM\Column(name: "title", type: "string", length: 255, nullable: false)]
     private string $title = '';
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="regisseur", type="string", length=127, nullable=false)
-     */
+    #[ORM\Column(name: "regisseur", type: "string", length: 127, nullable: false)]
     private string $regisseur = '';
 
-    /**
-     * @var \DateTimeImmutable
-     *
-     * @ORM\Column(name="publication", type="date_immutable", nullable=false)
-     */
+    #[ORM\Column(name: "publication", type: "date_immutable", nullable: false)]
     private \DateTimeImmutable $publication;
 
-    /**
-     * @var \DateTimeImmutable
-     *
-     * @ORM\Column(name="created", type="datetime_immutable", nullable=false)
-     */
+    #[ORM\Column(name: "created", type: "datetime_immutable", nullable: false)]
     private \DateTimeImmutable $created;
 
-    /**
-     * @var User
-     *
-     * @ORM\ManyToOne(targetEntity="KaiBoerner\MovieDb\Entity\User")
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="created_by", referencedColumnName="id")
-     * })
-     */
+    #[ORM\ManyToOne(targetEntity: "KaiBoerner\MovieDb\Entity\User")]
+    #[ORM\JoinColumn(name: "created_by", referencedColumnName: "id")]
     private User $createdBy;
 
 
