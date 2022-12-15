ab6730614f22584152244a1defbbeeb17007206e
Kai Börner <kai.boerner@web.de>
2022-12-15 16:32:05 +0100

    MovieController: use MessageQueue for messages and Application for redirect

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -3,9 +3,11 @@
 namespace KaiBoerner\MovieDb\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use KaiBoerner\MovieDb\Application;
 use KaiBoerner\MovieDb\Entity\Movie;
 use KaiBoerner\MovieDb\Security\SecurityInterface;
 use KaiBoerner\MovieDb\Templating\TemplateEngine;
+use KaiBoerner\MovieDb\Util\MessageQueue;
 
 /**
  * Controller for CRUD of movies
@@ -13,7 +15,9 @@ use KaiBoerner\MovieDb\Templating\TemplateEngine;
 final class MovieController
 {
     public function __construct(
+        private Application $application,
         private EntityManagerInterface $entityManager,
+        private MessageQueue $messageQueue,
         private SecurityInterface $security,
         private TemplateEngine $templateEngine
     )
@@ -22,23 +26,25 @@ final class MovieController
     public function newAction(): void
     {
         if (!$this->security->isLoggedIn()) {
-            header('Location: ?');
-            exit();
+            $this->messageQueue->addErrorMessage("Sie sind nicht angemeldet.");
+            $this->application->redirect('security', 'login');
         }
 
-        $messages = [];
         $movie = new Movie();
         if (isset($_POST['movie']) && is_array($_POST['movie'])) {
             $movie->acceptFormData($_POST['movie']);
             if (!$movie->isValid()) {
-                $messages[] = ['type' => 'error', 'text' => 'Fehlerhafte Daten'];
+                $this->messageQueue->addErrorMessage("Der Film konnte nicht gespeichert werden.");
             } elseif (!$movie->isUnique($this->entityManager)) {
-                $messages[] = ['type' => 'error', 'text' => 'Der Film ist bereits im Archiv'];
+                $this->messageQueue->addErrorMessage("Der Film ist bereits eingetragen");
+            } else {
+                $this->entityManager->persist($movie);
+                $this->entityManager->flush();
+                $this->messageQueue->addSuccessMessage("Der Film wurde eingetragen.");
+                $this->application->redirect('index', 'index');
             }
         }
-        $this->templateEngine->render('movie/new.html', [
-            'messages' => $messages,
-            'movie' => $movie
-        ]);
+        
+        $this->templateEngine->render('movie/new.html', ['movie' => $movie]);
     }
 }
