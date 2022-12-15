ca22425f801f4edbef3d1629a9d7a287571b40c2
Kai Börner <kai.boerner@web.de>
2022-12-15 16:20:31 +0100

    src/Application.php

--- src/Application.php
+++ src/Application.php
@@ -2,27 +2,61 @@
 
 namespace KaiBoerner\MovieDb;
 
-use KaiBoerner\MovieDb\Controller\IndexController;
-
 
 /**
  * entry point of the moviedb application
  */
 final class Application
 {
+    private function getActionMethod(string $action): string
+    {
+        $action = lcfirst(implode('', array_map('ucfirst', preg_split('/[^a-z0-9]/', strtolower($action)))));
+
+        return $action . 'Action';
+    }
+
+    public function getController(string $controllerName): object
+    {
+        return getContainer()->get($this->getControllerClass($controllerName));
+    }
+
+    public function getControllerClass(string $controllerName): string
+    {
+        $controllerName = implode('', array_map('ucfirst', preg_split('/[^a-z0-9]/', strtolower($controllerName))));
+
+        $controllerClass = "\\KaiBoerner\\MovieDb\\Controller\\${controllerName}Controller";
+
+        if (!class_exists($controllerClass)) {
+            throw new \Exception("controller NOT FOUND: '$controllerName'");
+        }
+
+        return $controllerClass;
+    }
+
     public function redirect(string $controllerName, string $action): void
     {
+        $controller = $this->getController($controllerName);
+        $method = $this->getActionMethod($action);
+
+        if (!is_callable([$controller, $method])) {
+            throw new \Exception("Invalid controller action: '$controllerName', '$action'");
+        }
+
         header("Location: ?controller=$controllerName&action=$action");
         exit();
     }
 
     public function run(string $controllerName, string $action): void
     {
-        $controllerName = implode('', array_map('ucfirst', preg_split('/[^a-z0-9]/', strtolower($controllerName))));
-        $controller = getContainer()->get("\\KaiBoerner\\MovieDb\\Controller\\${controllerName}Controller");
+        $controller = $this->getController($controllerName);
+        $method = $this->getActionMethod($action);
 
-        $action = lcfirst(implode('', array_map('ucfirst', preg_split('/[^a-z0-9]/', strtolower($action)))));
+        if (!is_callable([$controller, $method])) {
+            throw new \Exception("Invalid controller action: '$controllerName', '$action'");
+        }
 
-        $controller->{$action . 'Action'}();
+        $controller->$method();
+
+        exit();
     }
 }
