01cac95839550bfa63a0b9af24bf6256a27725f5
Kai Börner <kai.boerner@web.de>
2023-01-09 16:02:20 +0100

    src/EventListener/SetCreatedBy.php: prePersist event instead of onFlush

--- src/EventListener/SetCreatedBy.php
+++ src/EventListener/SetCreatedBy.php
@@ -2,35 +2,35 @@
 
 namespace KaiBoerner\MovieDb\EventListener;
 
-use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Event\OnFlushEventArgs;
+use Doctrine\ORM\Event\PrePersistEventArgs;
 use Doctrine\ORM\Events;
-use Doctrine\ORM\Mapping\Entity;
 use KaiBoerner\MovieDb\Entity\HasCreatedBy;
 use KaiBoerner\MovieDb\Security\SecurityInterface;
 use KaiBoerner\MovieDb\Security\UserInterface;
 
 
-final class SetCreatedBy implements EventSubscriber
+final class SetCreatedBy
 {
     public function __construct(
-        EntityManagerInterface $entityManager,
+        private EntityManagerInterface $entityManager,
         private SecurityInterface $security
     ) {
-        $entityManager->getEventManager()->addEventSubscriber($this);
+        $this->entityManager->getEventManager()->addEventListener(Events::prePersist, $this);
     }
 
-    /**
-    * {@inheritDoc}
-    */
-    public function getSubscribedEvents(): array
+    public function prePersist(PrePersistEventArgs $event): void
     {
-        return [ Events::onFlush ];
-    }
+        $entity = $event->getObject();
 
-    public function onFlush(OnFlushEventArgs $event): void
-    {
+        if (!($entity instanceof HasCreatedBy)) {
+            return;
+        }
+
+        if (null !== $entity->getCreatedBy()) {
+            return;
+        }
+
         if (!$this->security->isLoggedIn()) {
             return;
         }
@@ -40,30 +40,10 @@ final class SetCreatedBy implements EventSubscriber
             return;
         }
 
-        $entityManager = $event->getObjectManager();
-        $unitOfWork = $entityManager->getUnitOfWork();
-
-        $setCreatedBy = function (HasCreatedBy $entity) use ($entityManager, $unitOfWork, $user) {
-            if (null !== $entity->getCreatedBy()) {
-                return;
-            }
-            $entity->setCreatedBy($user);
-            $unitOfWork->recomputeSingleEntityChangeSet(
-                $entityManager->getClassMetadata(get_class($entity)),
-                $entity
-            );
-        };
-
-        foreach ($unitOfWork->getScheduledEntityInsertions() as $entity) {
-            if ($entity instanceof HasCreatedBy) {
-                $setCreatedBy($entity);
-            }
-        }
-
-        foreach ($unitOfWork->getScheduledEntityUpdates() as $entity) {
-            if ($entity instanceof HasCreatedBy) {
-                $setCreatedBy($entity);
-            }
-        }
+        $entity->setCreatedBy($user);
+        $this->entityManager->getUnitOfWork()->recomputeSingleEntityChangeSet(
+            $this->entityManager->getClassMetadata(get_class($entity)),
+            $entity
+        );
     }
 }
